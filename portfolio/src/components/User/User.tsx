b29d6ac8ffc79c6c979e74cc35fc75354ba832de
vfs <vfernandezs@fi.uba.ar>
2025-04-25 15:30:55 -0300

    added paragraph brakes

--- portfolio/src/components/User/User.tsx
+++ portfolio/src/components/User/User.tsx
@@ -4,9 +4,9 @@ export const User = ()=>{
     <section>
      <div>
         <h1> Hello, I'm Victoria</h1>
-        <p> I’m a software developer with 4 years of professional experience across a wide range of technologies.
+        <p> I’m a software developer with 4 years of professional experience across a wide range of technologies.<br/>
             While I consider myself a jack of all trades, I’ve worked on everything from embedded systems and Arduino, to frontend development, REST API hosting, and more recently, data engineering and large language models.
-            Let’s connect if you’d like to learn more about what I do! 
+            <br/>Let’s connect if you’d like to learn more about what I do! 
         </p>
      </div>
     </section>
