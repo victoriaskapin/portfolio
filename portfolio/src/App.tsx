4b8ee13eb3bff7a0810b6246af46cc1597ce8cc0
vfs <vfernandezs@fi.uba.ar>
2025-04-25 14:02:22 -0300

    added assets

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,13 +1,13 @@
-
-import './App.css'
+import styles from './App.module.css';
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
 
 
   return (
-    <>
-      hello world
-    </>
+    <div className={styles.App}>
+      <Navbar/>
+    </div>
   )
 }
 
